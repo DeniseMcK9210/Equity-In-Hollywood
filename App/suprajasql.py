c8b5a98fd0b1addb94c1b86368cb4e14a3b3ffb4
Supraja Sharma <supraja.sharma16@gmail.com>
2019-10-23 15:12:55 -0400

    pushing sqlite file

---
+++ App/suprajasql.py
@@ -0,0 +1,21 @@
+#Import dependencies
+import pandas as pd
+import csv, sqlite3
+from sqlite3 import Error
+
+#Function to connect to database
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return conn
+
